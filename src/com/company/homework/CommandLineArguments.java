94fb03c3c0dcb007855876dc01525df6b63055c9
uslavii <svslavka@ukr.net>
2020-02-23 17:33:03 +0200

    completed CommandLineArguments.java

--- src/com/company/homework/CommandLineArguments.java
+++ src/com/company/homework/CommandLineArguments.java
@@ -2,9 +2,23 @@ package com.company.homework;
 
 public class CommandLineArguments {
     public static void main(String[] args) {
-        System.out.println("123");
-
+        float result;
+        float num1 = Float.parseFloat(args[0]);
+        float num2 = Float.parseFloat(args[2]);
+        if (args[1].equals("+")) {
+            result = num1 + num2;
+            System.out.println(args[0] + " " + args[1] + " " + args[2] + " = " + result);
+        } else if (args[1].equals("-")) {
+            result = num1 - num2;
+            System.out.println(args[0] + " " + args[1] + " " + args[2] + " = " + result);
+        } else if (args[1].equals("/")) {
+            result = num1 / num2;
+            System.out.println(args[0] + " " + args[1] + " " + args[2] + " = " + result);
+        } else if (args[1].equals("*")) {
+            result = num1 * num2;
+            System.out.println(args[0] + " " + args[1] + " " + args[2] + " = " + result);
+        } else {
+            System.out.println("impossible to complete such a task");
+        }
     }
-
-
 }
