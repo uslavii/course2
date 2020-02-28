77dbf4410b1fddc2cc4c216811c8a95a848e6434
uslavii <svslavka@ukr.net>
2020-02-28 19:46:44 +0200

    completed task with constructors

---
+++ src/com/company/homework/сonstructors/Cat.java
@@ -0,0 +1,56 @@
+package com.company.homework.сonstructors;
+
+public class Cat {
+
+    public String name = "unknown";
+    public float weight = 1;
+    public int age = 1;
+    public String color = "White";
+    public String address = "homeless";
+
+    public Cat() {
+        name = "barsik";
+        weight = 3;
+        age = 1;
+        color = "White";
+        address = "homeless";
+
+
+    }
+
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public Cat(String name, int weight, int age) {
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Cat(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Cat(int weight, String color, String address) {
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
+    }
+
+    public void print() {
+        System.out.println(this.name);
+        System.out.println(this.weight);
+        System.out.println(this.age);
+        System.out.println(this.color);
+        System.out.println(this.address);
+    }
+
+}
---
+++ src/com/company/homework/сonstructors/Main.java
@@ -0,0 +1,21 @@
+package com.company.homework.сonstructors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.print();
+        Cat cat1 = new Cat("vasiliy");
+        cat1.print();
+        Cat cat2 = new Cat("Tom",1,2);
+        cat2.print();
+        Cat cat3 = new Cat("Jerry", 1) ;
+        cat3.print();
+        Cat cat4 = new Cat(2,"Black");
+        cat4.print();
+        Cat cat5 = new Cat(2,"Grey","dom 139");
+        cat5.print();
+
+    }
+
+}
