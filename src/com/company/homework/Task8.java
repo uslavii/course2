638185d34ab9ed3e353220e0bd3d17034f87196c
svato <svslavka@ukr.net>
2020-03-19 10:28:11 +0200

    task 8 changed

--- src/com/company/homework/Task8.java
+++ src/com/company/homework/Task8.java
@@ -6,19 +6,13 @@ import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 
 public class Task8 {
-
     static class ConsoleReader {
-        public String readString() throws IOException {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String result = reader.readLine();
-            return result;
+        public String readString(String console) throws IOException {
+            return console;
         }
 
-        public int readInt() throws IOException {
+        public int readInt(String console) throws IOException {
             Pattern pattern = Pattern.compile("[0-9]");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("введите число");
-            String console = reader.readLine();
             if (console.matches(String.valueOf(pattern))) {
                 System.out.println("вы ввели неправильные символы");
                 return 0;
@@ -27,10 +21,7 @@ public class Task8 {
             return result;
         }
 
-        public double readDouble() throws IOException {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("введите дробное число");
-            String console = reader.readLine();
+        public double readDouble(String console) throws IOException {
             if (console.matches("[.]")) {
                 System.out.println("вы ввели не дробное число. используйте другой метод");
                 return 0;
@@ -39,10 +30,7 @@ public class Task8 {
             return result;
         }
 
-        public void readLn() throws IOException {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("нажмите enter");
-            String console = readString();
+        public void readLn(String console) throws IOException {
             if (console.matches("/n")) {
                 System.out.println("неверно!");
             }
@@ -51,10 +39,19 @@ public class Task8 {
     }
 
     public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ConsoleReader consoleReader = new ConsoleReader();
-        System.out.println(consoleReader.readString());
-        System.out.println(consoleReader.readInt());
-        System.out.println(consoleReader.readDouble());
-        consoleReader.readLn();
+        System.out.println("введите любой текст");
+        String console = reader.readLine();
+        System.out.println(consoleReader.readString(console));
+        System.out.println("введите число");
+        console = reader.readLine();
+        System.out.println(consoleReader.readInt(console));
+        System.out.println("введите дробное число");
+        console = reader.readLine();
+        System.out.println(consoleReader.readDouble(console));
+        System.out.println("нажмите enter");
+        console = reader.readLine();
+        consoleReader.readLn(console);
     }
 }
